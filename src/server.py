0bcbd054145fef48d4850068775675480111858b
Moises Herrera <moisesherrera@Moisess-MacBook-Air.local>
2024-02-13 22:07:14 -0600

    requirments with version numbers

--- src/server.py
+++ src/server.py
@@ -5,7 +5,13 @@ from src.__init__ import create_app
 load_dotenv.load_dotenv()
 app = create_app(os.getenv("CONFIG_MODE"))
 
+# — bind 0.0.0.0:$PORT
 
+@app.route("/api", methods=["GET"])
+def index():
+    return {
+        "Server Home"
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
